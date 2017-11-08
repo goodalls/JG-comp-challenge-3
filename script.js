7a0ecdb17619d3ac3942bcba0482b78a05d18e02
Jeff Goodall <goodallhj@gmail.com>
2017-11-08 10:06:05 -0700

    added circles with css

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main-content").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main-content").style.marginLeft = "0";
 }
